9e847f68e67fef002628fd587abb3c509308f753
larsewi <lars.erik.wik@northern.tech>
2022-02-09 09:08:10 +0100

    Created debug messenger
    Signed-off-by: larsewi <lars.erik.wik@northern.tech>

--- src/leech.c
+++ src/leech.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <assert.h>
 
 #include "leech.h"
 
-void PrintHello(const char *from)
+#define BUFFER_SIZE 4096UL
+
+#define LOG_DEBUG(instance, ...) logDebugMessenger(instance, LCH_DEBUG_MESSAGE_TYPE_DEBUG_BIT, __VA_ARGS__)
+#define LOG_VERBOSE(instance, ...) logDebugMessenger(instance, LCH_DEBUG_MESSAGE_TYPE_VERBOSE_BIT, __VA_ARGS__)
+#define LOG_INFO(instance, ...) logDebugMessenger(instance, LCH_DEBUG_MESSAGE_TYPE_INFO_BIT, __VA_ARGS__)
+#define LOG_WARNING(instance, ...) logDebugMessenger(instance, LCH_DEBUG_MESSAGE_TYPE_WARNING_BIT, __VA_ARGS__)
+#define LOG_ERROR(instance, ...) logDebugMessenger(instance, LCH_DEBUG_MESSAGE_TYPE_ERROR_BIT, __VA_ARGS__)
+
+
+static void logDebugMessenger(const LCH_Instance *instance, unsigned char severity, const char *format, ...)
 {
-    printf("Hello from %s!\n", from);
+    assert(instance != NULL);
+    if (instance->debugMessenger == NULL)
+    {
+        return;
+    }
+    if ((instance->debugMessenger->severity & severity) == 0)
+    {
+        return;
+    }
+    assert(instance->debugMessenger->callback != NULL);
+
+    va_list ap;
+    va_start(ap, format);
+
+    char message[BUFFER_SIZE];
+    int size = vsnprintf(message, sizeof(message), format, ap);
+    assert(size >= 0);
+
+    va_end(ap);
+
+    if ((unsigned long) size >= BUFFER_SIZE)
+    {
+        LOG_WARNING(instance, "Debug messenger output truncated (%lu > %lu)", size, BUFFER_SIZE);
+    }
+
+    instance->debugMessenger->callback(severity, message);
+}
+
+void LCH_TestFunc(const LCH_Instance *instance)
+{
+    LOG_DEBUG(instance, "This is a debug message");
+    LOG_VERBOSE(instance, "This is a verbose message");
+    LOG_INFO(instance, "This is a info message");
+    LOG_WARNING(instance, "This is a warning message");
+    LOG_ERROR(instance, "This is an error message");
 }
--- src/leech.h
+++ src/leech.h
@@ -1,6 +1,60 @@
 #ifndef _LEECH_H
 #define _LEECH_H
 
-void PrintHello(const char *from);
+#include <assert.h>
 
-#endif
+#define LCH_DEBUG_MESSAGE_TYPE_DEBUG_BIT    (1 << 0)
+#define LCH_DEBUG_MESSAGE_TYPE_VERBOSE_BIT  (1 << 1)
+#define LCH_DEBUG_MESSAGE_TYPE_INFO_BIT     (1 << 2)
+#define LCH_DEBUG_MESSAGE_TYPE_WARNING_BIT  (1 << 3)
+#define LCH_DEBUG_MESSAGE_TYPE_ERROR_BIT    (1 << 4)
+
+#define LCH_COLOR_RED     "\x1b[31m"
+#define LCH_COLOR_GREEN   "\x1b[32m"
+#define LCH_COLOR_YELLOW  "\x1b[33m"
+#define LCH_COLOR_BLUE    "\x1b[34m"
+#define LCH_COLOR_CYAN    "\x1b[36m"
+#define LCH_COLOR_RESET   "\x1b[0m"
+
+
+typedef struct LCH_DebugMessenger
+{
+    unsigned char severity;
+    void (*callback)(unsigned char, const char *);
+} LCH_DebugMessenger;
+
+typedef struct LCH_Instance
+{
+    LCH_DebugMessenger *debugMessenger;
+} LCH_Instance;
+
+void LCH_DebugMessengerCallback(unsigned char severity, const char *message)
+{
+    assert(message != NULL);
+    int rc = 0;
+    switch (severity)
+    {
+    case LCH_DEBUG_MESSAGE_TYPE_DEBUG_BIT:
+        rc = fprintf(stdout, "[" LCH_COLOR_BLUE "DBUG" LCH_COLOR_RESET "]: %s\n", message);
+        break;
+    case LCH_DEBUG_MESSAGE_TYPE_VERBOSE_BIT:
+        rc = fprintf(stdout, "[" LCH_COLOR_CYAN "VERB" LCH_COLOR_RESET "]: %s\n", message);
+        break;
+    case LCH_DEBUG_MESSAGE_TYPE_INFO_BIT:
+        rc = fprintf(stdout, "[" LCH_COLOR_GREEN "INFO" LCH_COLOR_RESET "]: %s\n", message);
+        break;
+    case LCH_DEBUG_MESSAGE_TYPE_WARNING_BIT:
+        rc = fprintf(stdout, "[" LCH_COLOR_YELLOW "WARN" LCH_COLOR_RESET "]: %s\n", message);
+        break;
+    case LCH_DEBUG_MESSAGE_TYPE_ERROR_BIT:
+        rc = fprintf(stderr, "[" LCH_COLOR_RED "ERRR" LCH_COLOR_RESET "]: %s\n", message);
+        break;
+    default:
+        break;
+    }
+    assert(rc >= 0);
+}
+
+void LCH_TestFunc(const LCH_Instance *instance);
+
+#endif // _LEECH_H
