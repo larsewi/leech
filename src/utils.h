2f1c88b817c80e1dcaa99a04c6bd0de115a61321
Lars Erik Wik <lars.erik.wik@northern.tech>
2022-09-09 17:51:59 +0200

    Add doc strings to utils.h

--- src/utils.h
+++ src/utils.h
@@ -41,25 +41,68 @@ size_t LCH_ListLength(const LCH_List *self);
 size_t LCH_DictLength(const LCH_Dict *self);
 
 /**
- * Append item to a list.
- * @param[in] self
- * @param[in] value
- * @param[in] destroy
- * @param[out] success
+ * Append value to a list.
+ * List takes ownership of value.
+ * @param[in] self pointer to dict.
+ * @param[in] value value to append.
+ * @param[in] destroy pointer to value destroy function.
+ * @param[out] success true if success.
  */
 bool LCH_ListAppend(LCH_List *self, void *value, void (*destroy)(void *));
 
+/**
+ * Check if dict has key.
+ * @param[in] self pointer to dict.
+ * @param[in] key key to check.
+ * @param[out] present true if key is present.
+ */
 bool LCH_DictHasKey(const LCH_Dict *self, const char *key);
 
+/**
+ * Set value if key is present or add key value pair.
+ * Dict takes ownership of value.
+ * @param[in] self pointer to dict.
+ * @param[in] key key to set.
+ * @param[in] value value to set.
+ * @param[in] destroy pointer to value destroy function.
+ */
 bool LCH_DictSet(LCH_Dict *self, const char *key, void *value,
                  void (*destroy)(void *));
 
+/**
+ * Get list item.
+ * @param[in] self pointer to list.
+ * @param[in] index index of item.
+ * @param[out] item item from list.
+ */
 void *LCH_ListGet(const LCH_List *self, size_t index);
+
+/**
+ * Get dict value.
+ * @param[in] self pointer to dict.
+ * @param[in] key key assosiated with value.
+ * @param[in] value value assosiated with key.
+ */
 void *LCH_DictGet(const LCH_Dict *self, const char *key);
 
+/**
+ * Destroy list and contents.
+ * @param[in] self pointer to list.
+ */
 void LCH_ListDestroy(LCH_List *self);
+
+/**
+ * Destroy dict and contents.
+ * @param[in] self pointer to dict.
+ */
 void LCH_DictDestroy(LCH_Dict *self);
 
+/**
+ * Split a string with delimitor.
+ * @param[in] str string to split.
+ * @param[in] del delimitor.
+ * @param[out] list list of substrings.
+ */
 LCH_List *LCH_SplitString(const char *str, const char *del);
 
 #endif // _LEECH_UTILS_H
