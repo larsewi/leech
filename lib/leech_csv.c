cac092f8ffa3b7f9dac20c22b4e6c1e3c3e79b7c
= <=>
2022-12-11 16:05:56 +0100

    Implemented LCH_TableWriteCallbackCSV

--- lib/leech_csv.c
+++ lib/leech_csv.c
@@ -17,12 +17,14 @@ LCH_List *LCH_TableReadCallbackCSV(const char *const locator) {
   size_t size;
   if (!LCH_FileSize(file, &size)) {
     LCH_LOG_ERROR("Failed to get size of file '%s'", locator);
+    fclose(file);
     return NULL;
   }
 
   char buffer[size];
   if (fread((void *)buffer, 1, size, file) != size) {
     LCH_LOG_ERROR("Failed to read file '%s': %s", locator, strerror(errno));
+    fclose(file);
     return NULL;
   }
   fclose(file);
@@ -40,5 +42,39 @@ bool LCH_TableWriteCallbackCSV(const char *const locator,
                                const LCH_List *const table) {
   assert(locator != NULL);
   assert(table != NULL);
-  return false;
+
+  LCH_Buffer *const buffer = LCH_ComposeCSV(table);
+  if (buffer == NULL) {
+    LCH_LOG_ERROR("Failed to compose CSV for file '%s'", locator);
+    return false;
+  }
+
+  char *const csv = LCH_BufferGet(buffer);
+  if (csv == NULL) {
+    LCH_LOG_ERROR(
+        "Failed to get string from buffer after composing CSV for file '%s'",
+        locator);
+    return false;
+  }
+
+  const size_t length = LCH_BufferLength(buffer);
+  LCH_BufferDestroy(buffer);
+
+  FILE *const file = fopen(locator, "w");
+  if (file == NULL) {
+    LCH_LOG_ERROR("Failed to open file '%s' for writing: %s", locator,
+                  strerror(errno));
+    return false;
+  }
+
+  if (fwrite(csv, 1, length, file) != length) {
+    LCH_LOG_ERROR("Failed to write composed CSV (%zu bytes) to file '%s': %s",
+                  length, locator, strerror(errno));
+    fclose(file);
+    return false;
+  }
+
+  fclose(file);
+  free(csv);
+  return true;
 }
--- tests/check_leech_csv.c
+++ tests/check_leech_csv.c
@@ -52,11 +52,68 @@ START_TEST(test_LCH_TableReadCallbackCSV) {
 }
 END_TEST
 
+START_TEST(test_LCH_TableWriteCallbackCSV) {
+  char *content[][LCH_BUFFER_SIZE] = {
+      {(char *)"firstname", (char *)"lastname", (char *)"born"},
+      {(char *)"Paul", (char *)"McCartney", (char *)"1942"},
+      {(char *)"Ringo", (char *)"Starr", (char *)"1940"},
+      {(char *)"John", (char *)"Lennon", (char *)"1940"},
+      {(char *)"George", (char *)"Harrison", (char *)"1943"},
+  };
+
+  LCH_List *const table = LCH_ListCreate();
+  ck_assert_ptr_nonnull(table);
+
+  for (size_t i = 0; i < 5; i++) {
+    LCH_List *record = LCH_ListCreate();
+    ck_assert_ptr_nonnull(record);
+
+    for (size_t j = 0; j < 3; j++) {
+      char *const field = strdup(content[i][j]);
+      ck_assert_ptr_nonnull(field);
+
+      ck_assert(LCH_ListAppend(record, field, free));
+    }
+
+    ck_assert(LCH_ListAppend(table, record, (void (*)(void *))LCH_ListDestroy));
+  }
+
+  ck_assert(LCH_TableWriteCallbackCSV("sample.csv", table));
+  LCH_ListDestroy(table);
+
+  FILE *const file = fopen("sample.csv", "r");
+  ck_assert_ptr_nonnull(file);
+
+  size_t size;
+  ck_assert(LCH_FileSize(file, &size));
+
+  char actual[size];
+  ck_assert_int_eq(fread(actual, 1, size, file), size);
+
+  fclose(file);
+  ck_assert_int_eq(remove("sample.csv"), 0);
+
+  const char expect[] =
+      "firstname,lastname,born\r\n"
+      "Paul,McCartney,1942\r\n"
+      "Ringo,Starr,1940\r\n"
+      "John,Lennon,1940\r\n"
+      "George,Harrison,1943";
+
+  ck_assert_str_eq(actual, expect);
+}
+END_TEST
+
 Suite *LeechCSVSuite(void) {
   Suite *s = suite_create("leech_csv.c");
   {
     TCase *tc = tcase_create("LCH_TableReadCallbackCSV");
     tcase_add_test(tc, test_LCH_TableReadCallbackCSV);
+    suite_add_tcase(s, tc);
+  }
+  {
+    TCase *tc = tcase_create("LCH_TableWriteCallbackCSV");
+    tcase_add_test(tc, test_LCH_TableWriteCallbackCSV);
     suite_add_tcase(s, tc);
   }
   return s;
