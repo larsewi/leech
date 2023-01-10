2ae4d0b2a552906d6cf713f8ed1bd815f9dff68c
= <=>
2023-01-10 23:45:26 +0100

    head

--- lib/head.c
+++ lib/head.c
@@ -6,26 +6,23 @@
 #include "leech.h"
 #include "utils.h"
 
-// char *LCH_HeadGet(const char *const work_dir) {
-//   char path[PATH_MAX];
-//   if (!LCH_PathJoin(path, sizeof(path), 2, work_dir, "HEAD")) {
-//     return NULL;
-//   }
+char *LCH_HeadGet(const char *const work_dir) {
+  char path[PATH_MAX];
+  if (!LCH_PathJoin(path, sizeof(path), 2, work_dir, "HEAD")) {
+    return NULL;
+  }
 
-//   FILE *file = fopen(path, "r");
-//   if (file == NULL) {
-//     LCH_LOG_ERROR("Failed to read file '%s': %s", path, strerror(errno));
-//     return NULL;
-//   }
+  if (!LCH_IsRegularFile(path)) {
+    return strdup("0000000000000000000000000000000000000000");
+  }
 
-//   size_t size;
-//   if (!LCH_FileSize(file, &size)) {
-//     LCH_LOG_ERROR("Failed to get size of file '%s'.", file);
-//     fclose(file);
-//     return NULL;
-//   }
-//   return NULL;
-// }
+  char *head = LCH_ReadFile(path, NULL);
+  if (head == NULL) {
+    return NULL;
+  }
 
-// bool LCH_HeadSet(const char *const workdir, const char *const block_id) {
-// return false; }
+  return LCH_StringStrip(head, " \t\r\n");
+}
+
+bool LCH_HeadSet(const char *const workdir, const char *const block_id) {
+return false; }
