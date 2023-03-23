71c487205dfdbaf342f4505f96d5d671480bd428
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-03-23 11:56:19 +0100

    Don't use shell in simulate script
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- simulate.py
+++ simulate.py
@@ -13,11 +13,10 @@ HOSTS = {
 }
 
 class Event(ABC):
-    def __init__(self, hostname, hostkey, timestamp, workdir):
+    def __init__(self, hostname, hostkey, timestamp):
         self.hostname = hostname
         self.hostkey = hostkey
         self.timestamp = timestamp
-        self.workdir = workdir
 
     @abstractmethod
     def work(self):
@@ -27,47 +26,56 @@ class Event(ABC):
         return self.timestamp < other.timestamp
 
 class Commit(Event):
-    def __init__(self, hostname, hostkey, timestamp, workdir, tables):
-        super().__init__(hostname, hostkey, timestamp, workdir)
+    def __init__(self, hostname, hostkey, timestamp, tables):
+        super().__init__(hostname, hostkey, timestamp)
         self.tables = tables
 
     def work(self):
         print(f"*** Commit {self.timestamp} {self.hostname} ***")
-        os.makedirs(os.path.join(self.workdir, ".leech"), exist_ok=True)
+        os.makedirs(os.path.join("simulate", self.hostname, ".leech"), exist_ok=True)
 
         for table in self.tables:
-            shutil.copy(table, os.path.join(self.workdir, ".leech"))
+            shutil.copy(table, os.path.join("simulate", self.hostname, ".leech"))
 
-        command = "cd %s && ../../bin/leech --info commit" % self.workdir
-        p = subprocess.run(command, shell=True)
+        cwd = os.getcwd()
+        os.chdir(os.path.join("simulate", self.hostname))
+        command = ["../../bin/leech", "--info", "commit"]
+        p = subprocess.run(command)
+        os.chdir(cwd)
         if p.returncode != 0:
-            print("Command '%s' returned %d" % (command, p.returncode))
+            print("Command '%s' returned %d" % (" ".join(command), p.returncode))
             exit(1)
 
 class Patch(Event):
-    def __init__(self, hostname, hostkey, timestamp, workdir):
-        super().__init__(hostname, hostkey, timestamp, workdir)
+    def __init__(self, hostname, hostkey, timestamp):
+        super().__init__(hostname, hostkey, timestamp)
 
     def work(self):
         print(f"*** Delta {self.timestamp} {self.hostname} ***")
         lastseen_block = "0000000000000000000000000000000000000000"
-        lastseen_block_path = os.path.join(self.workdir, "../hub/.leech", self.hostkey)
+        lastseen_block_path = os.path.join("simulate/hub/.leech", self.hostkey)
+
         if os.path.exists(lastseen_block_path):
             with open(lastseen_block_path, "r") as f:
                 lastseen_block = f.read().strip()
-        patch_file = os.path.join(self.workdir, ".leech", "%s_patchfile" % self.timestamp.timestamp())
+        patch_file = os.path.join(os.getcwd(), "simulate", self.hostname, f".leech/{int(self.timestamp.timestamp())}_patchfile")
 
-        command = "cd %s && ../../bin/leech --info delta --block %s --file %s" % (self.workdir, lastseen_block, patch_file)
-        p = subprocess.run(command, shell=True)
+        cwd = os.getcwd()
+        os.chdir(os.path.join("simulate", self.hostname))
+        command = ["../../bin/leech", "--info", "delta", "--block", lastseen_block, "--file", patch_file]
+        p = subprocess.run(command)
+        os.chdir(cwd)
         if p.returncode != 0:
-            print("Command '%s' returned %d" % (command, p.returncode))
+            print("Command '%s' returned %d" % (" ".join(command), p.returncode))
             exit(1)
 
         print(f"*** Patch {self.timestamp} {self.hostname} ***")
-        command = "cd %s && ../../bin/leech --info patch --field %s --value %s --file %s" %(os.path.join(os.getcwd(), "simulate/hub"), "host_key", self.hostkey, patch_file)
-        p = subprocess.run(command, shell=True)
+        os.chdir("simulate/hub")
+        command = ["../../bin/leech", "--info", "patch", "--field", "uid", "--value", self.hostkey, "--file", patch_file]
+        p = subprocess.run(command)
+        os.chdir(cwd)
         if p.returncode != 0:
-            print("Command '%s' returned %d" % (command, p.returncode))
+            print("Command '%s' returned %d" % (" ".join(command), p.returncode))
             exit(1)
 
 def main():
@@ -76,8 +84,6 @@ def main():
     subprocess.run("rm -rf simulate/**/.leech", shell=True)
 
     for hostname, hostkey in HOSTS.items():
-        workdir = os.path.join(os.getcwd(), "simulate", hostname)
-
         for dirpath, _, filenames in os.walk(os.path.join("simulate", hostname)):
             if len(filenames) == 0:
                 continue
@@ -85,12 +91,12 @@ def main():
             if "table_dumps" in dirpath:
                 timestamp = datetime.fromtimestamp(int(dirpath.split("/")[-1]))
                 tables = [os.path.join(dirpath, f) for f in filenames]
-                event = Commit(hostname, hostkey, timestamp, workdir, tables)
+                event = Commit(hostname, hostkey, timestamp, tables)
                 events.append(event)
             elif "report_dumps" in dirpath:
                 for filename in filenames:
                     timestamp = datetime.fromtimestamp(int(filename.split("_")[0]))
-                    event = Patch(hostname, hostkey, timestamp, workdir)
+                    event = Patch(hostname, hostkey, timestamp)
                     events.append(event)
 
     events = sorted(events)
