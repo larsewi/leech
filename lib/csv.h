3ebd01e3f56417260382bfa32067c49a59a54a74
larsewi <lars.erik.wik@northern.tech>
2023-11-21 09:35:39 +0100

    Added doc strings for LCH_CSVCompose{Table,Record}

--- lib/csv.h
+++ lib/csv.h
@@ -14,8 +14,22 @@ LCH_List *LCH_CSVParseFile(const char *path);
 
 /****************************************************************************/
 
+/**
+ * Compose a CSV formatted string from a table (two dimentional list)
+ * @param buffer Buffer to hold the resulting string
+ * @param table two dimentional list containing table
+ * @note if `buffer` is `NULL`, then a new buffer is created
+ * @return `true` on success, else `false`
+ */
 bool LCH_CSVComposeTable(LCH_Buffer **buffer, const LCH_List *table);
 
+/**
+ * Compose a CSV formatted string from a record (one dimentional list)
+ * @param buffer Buffer to hold the resulting string
+ * @param table one dimentional list containing table
+ * @note if `buffer` is `NULL`, then a new buffer is created
+ * @return `true` on success, else `false`
+ */
 bool LCH_CSVComposeRecord(LCH_Buffer **buffer, const LCH_List *record);
 
 char *LCH_CSVComposeField(const char *const field);
