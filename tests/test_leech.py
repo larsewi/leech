cc8f3e7aadbbc15524bc2e2f01c692b2722185c4
Lars Erik Wik <lars.erik.wik@northern.tech>
2024-04-26 13:54:34 +0200

    Added test for purge command
    Ticket: CFE-4373
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/test_leech.py
+++ tests/test_leech.py
@@ -732,7 +732,7 @@ def test_leech_csv_binary(tmp_path):
     assert execute(command, True) == 0
 
 
-def test_leech_garbage_collect(tmp_path):
+def test_leech_purge(tmp_path):
     ##########################################################################
     # Create config
     ##########################################################################
@@ -773,6 +773,59 @@ def test_leech_garbage_collect(tmp_path):
     for _ in range(5):
         command = [bin_path, "--debug", f"--workdir={tmp_path}", "commit"]
         assert execute(command, True) == 0
+
+    assert len(os.listdir(os.path.join(tmp_path, "blocks"))) == 5
+
+    command = [bin_path, "--debug", f"--workdir={tmp_path}", "purge"]
+    assert execute(command, True) == 0
+
+    assert len(os.listdir(os.path.join(tmp_path, "blocks"))) == 3
+
+
+def test_leech_auto_purge(tmp_path):
+    ##########################################################################
+    # Create config
+    ##########################################################################
+
+    bin_path = os.path.join("bin", "leech")
+    leech_conf_path = os.path.join(tmp_path, "leech.json")
+    table_src_path = os.path.join(tmp_path, "beatles.src.csv")
+    table_dst_path = os.path.join(tmp_path, "beatles.dst.csv")
+
+    config = {
+        "version": "0.1.0",
+        "auto_purge": True,
+        "chain_length": 3,
+        "tables": {
+            "BTL": {
+                "primary_fields": ["first_name", "last_name"],
+                "subsidiary_fields": ["born"],
+                "source": {
+                    "params": table_src_path,
+                    "schema": "leech",
+                    "table_name": "beatles",
+                    "callbacks": "lib/.libs/leech_csv.so",
+                },
+                "destination": {
+                    "params": table_dst_path,
+                    "schema": "leech",
+                    "table_name": "beatles",
+                    "callbacks": "lib/.libs/leech_csv.so",
+                },
+            }
+        },
+    }
+    with open(leech_conf_path, "w") as f:
+        json.dump(config, f, indent=2)
+    print(f"Created leech config '{leech_conf_path}' with content:")
+    with open(leech_conf_path, "r") as f:
+        print(f.read())
+
+    for _ in range(5):
+        command = [bin_path, "--debug", f"--workdir={tmp_path}", "commit"]
+        assert execute(command, True) == 0
+
+    assert len(os.listdir(os.path.join(tmp_path, "blocks"))) == 3
 
 
 def test_leech_churn(tmp_path):
